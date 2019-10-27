0d606c74ad905c87ab1f981a4c023c3ad3118697
thatguyshzr <shahzar.ansari94@gmail.com>
2019-10-27 14:07:06 +0530

    name changed

---
+++ image_rename.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import os, string
+
+x=0
+for f in os.listdir('.'):	
+	if f.endswith('.jpg') | f.endswith('jpeg'):
+		str= string.ascii_lowercase[x]
+		os.rename(f,'Eid 6.06.19_{}.jpg'.format(str))
+		x= x+1
+
+
